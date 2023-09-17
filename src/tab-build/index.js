9cd1eafd7f34141c54f83dc05afebafb1ec204dc
Jdoggett50 <dillondoggett5@gmail.com>
2023-09-17 08:42:17 -0400

    recognized the beginnings of repetitive styling, general styling  added to index.css

--- src/tab-build/index.js
+++ src/tab-build/index.js
@@ -1,34 +1,16 @@
-import loadDom from "./page-load.js";
 import menuComponent from './menu.js';
 import dessertComponent from './dessert.js';
 import drinkComponent from './drink.js';
-import homeComponent from "./home.js";
-export {content};
+import pageLoad from './page-load.js';
 
 //this file is responsible for the tabbing behavior. The other files are responsible for creating and showing the tabs.
 //there needs to be a default 
 
+
 //there needs to be a templating function that says "I'm appending this to the dom" 
 
 //create 3 different buttons and have each button log what their supposed to be doing to the dom.
-
 const content = document.querySelector('#content');
+const body = document.querySelector('body');
 
-const dessertTab = document.createElement('button');
-dessertTab.textContent = "dessert";
-dessertTab.id = "dessert-tab";
-content.append(dessertTab);
-
-const drinkTab = document.createElement('button');
-drinkTab.textContent = "drinks";
-drinkTab.id = "drink-tab";
-content.append(drinkTab);
-
-const menuTab = document.createElement('button');
-menuTab.textContent = "menu";
-menuTab.id = "menu-tab";
-content.append(menuTab);
-
-menuTab.addEventListener('click', homeComponent);
-dessertTab.addEventListener('click', dessertComponent);
-drinkTab.addEventListener('click', drinkComponent);
+pageLoad(content)
--- src/tab-build/page-load.js
+++ src/tab-build/page-load.js
@@ -1,5 +1,10 @@
-export default loadDom
+export default pageLoad;
+import pageLoadStyle from '../tab-style/page-load.css';
+import indexStyles from '../tab-style/index.css';
     
-function loadDom(){
-    
+function pageLoad(el){
+    //this is resposible for creating a div and returning it with it's desired content.
+    const img = document.createElement('img');
+    img.classlist.add('img');
+    el.appendChild()
 }
