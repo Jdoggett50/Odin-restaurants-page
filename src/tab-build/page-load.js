b7115f1e1407ec6187de8c4f916725acb3689005
Jdoggett50 <dillondoggett5@gmail.com>
2023-09-19 07:52:57 -0400

    adjusted layout of main page

--- src/tab-build/page-load.js
+++ src/tab-build/page-load.js
@@ -1,7 +1,7 @@
 export default pageLoad;
 export {content}; 
 //page specific styling
-import pageLoadStyle from '../tab-style/page-load.css';
+import pageStyle from '../tab-style/page-load.css';
 //DRY styling
 import indexStyles from '../tab-style/index.css';
 
@@ -12,8 +12,18 @@ function pageLoad(el){
     //make header
     const header = document.createElement('header');
     const h1 = document.createElement('h1');
+    const div = document.createElement('div');
     h1.textContent = "Tropical Paradise";
+    div.classList.add('container');
+    const elArray = [header, div];
     header.appendChild(h1);
-    el.appendChild(header);
-    return el
+    // el.appendChild(header);
+    // el.appendChild(div);
+    return elArray.forEach(i => {
+        el.appendChild(i);
+    })
+}
+
+function loadElements() {
+
 }
