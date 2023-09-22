9513cebc1e5be87717f663ed53e806902322363e
Jdoggett50 <dillondoggett5@gmail.com>
2023-09-22 16:03:28 -0400

    cleaned up the pageload function for readability

--- src/tab-build/page-load.js
+++ src/tab-build/page-load.js
@@ -9,30 +9,24 @@ const content = document.querySelector('#content');
 
 //this will be the layout for the entire site. 
 function pageLoad(el){
-    //make header
+    //header and its contents
     const header = document.createElement('header');
     const h1 = document.createElement('h1');
-    const div = document.createElement('div');
-    const siteDetailsP = document.createElement('p');
     const menuTab = document.createElement('div');
     const drinkTab = document.createElement('div');
     const dessertTab = document.createElement('div');
 
-    div.classList.add('container');
-    siteDetailsP.textContent = "Welcome to our online store! We take this currency, that currency, tweedlebucks and casheroos! Please Call "
-    div.appendChild(siteDetailsP);
-    
+    //image container and it's akin scrolling container
+    const scrollingContainer = document.createElement('div');
+    const backgroundContainer = document.createElement('div');
     
     h1.textContent = "Tropical Paradise";
-    menuTab.classList.add('menu-tab');
-    menuTab.classList.add('selection');
+    
     menuTab.textContent = "Menu";
-    drinkTab.classList.add('drink-tab');
+
     drinkTab.textContent = "Drinks";
-    drinkTab.classList.add('selection');
-    dessertTab.classList.add('dessert-tab');
+
     dessertTab.textContent = "Desserts";
-    dessertTab.classList.add('selection')
 
 
     const headerArr = [h1,menuTab,drinkTab,dessertTab];
@@ -40,13 +34,11 @@ function pageLoad(el){
         header.appendChild(i);
     })
 
-    const elArray = [header, div];
+    const contentArr = [header, scrollingContainer, backgroundContainer];
 
-    return elArray.forEach(i => {
+    return contentArr.forEach(i => {
         el.appendChild(i);
     })
 }
 
-function generateHeader(){
-    
-}
+// "Welcome to our online store! We take this currency, that currency, tweedlebucks and casheroos! Please Call"
