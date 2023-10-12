9c498fde83042739269f2b1c1c1bdcbed80e9513
Jdoggett50 <dillondoggett5@gmail.com>
2023-10-12 15:45:27 -0400

    completed menu styling

--- src/tab-build/menu.js
+++ src/tab-build/menu.js
@@ -77,15 +77,18 @@ function setSpecialsContentContainer() {
     const specialsMenuItems = document.querySelectorAll('.special-menu-items');
     specialsMenuItems.forEach(el => appendGeneratedDivs(el, 2, 'specials-content'));
     giveDiffClasses('specials-content');
-    // setSpecialsTextBox1();
-    // setSpecialsTextBox2();
-    // setSpecialsTextBox3();
+    setSpecialsTextBox1();
+    setSpecialsTextBox2();
+    setSpecialsTextBox3();
 }
 
 function setSidesContentContainer() {
     const sideMenuItems = document.querySelectorAll('.side-menu-items');
     sideMenuItems.forEach(el => appendGeneratedDivs(el, 2, 'sides-content'));
     giveDiffClasses('sides-content');
+    setSidesTextBox1();
+    setSidesTextBox2();
+    setSidesTextBox3();
 }
 
 function setAppTextBox1() {
@@ -99,21 +102,75 @@ function setAppTextBox1() {
 }
 
 function setAppTextBox2() {
-    const appContent0 = document.querySelector('.app-content-2');
-    const appContent1 = document.querySelector('.app-content-3');
+    const appContent2 = document.querySelector('.app-content-2');
+    const appContent3 = document.querySelector('.app-content-3');
     const h3 = document.createElement('h3');
     const p = document. createElement('p');
-    appContent0.appendChild(h3).textContent = "Quisque Vestibulum \r\n Tasting Platter";
-    appContent1.appendChild(p).textContent = "Nullam aliquet eros ac metus cursus, vel condimentum justo tincidunt. Lorem ipsum crunch on our delectable assortment of lorem ipsum-infused crispy bites.";
+    appContent2.appendChild(h3).textContent = "Quisque Vestibulum \r\n Tasting Platter";
+    appContent3.appendChild(p).textContent = "Nullam aliquet eros ac metus cursus, vel condimentum justo tincidunt. Lorem ipsum crunch on our delectable assortment of lorem ipsum-infused crispy bites.";
 }
 
 function setAppTextBox3() {
-    const appContent0 = document.querySelector('.app-content-4');
-    const appContent1 = document.querySelector('.app-content-5');
+    const appContent4 = document.querySelector('.app-content-4');
+    const appContent5 = document.querySelector('.app-content-5');
     const h3 = document.createElement('h3');
     const p = document. createElement('p');
-    appContent0.appendChild(h3).textContent = "Quam Blandit \r\n Quinoa Bites";
-    appContent1.appendChild(p).textContent = "Crispy quinoa and cheese bites, served with a tangy Lorem Ipsum aioli. Duis non tortor a lectus consectetur fringilla. Sed lobortis arcu ut vestibulum elementum.";
+    appContent4.appendChild(h3).textContent = "Quam Blandit \r\n Quinoa Bites";
+    appContent5.appendChild(p).textContent = "Crispy quinoa and cheese bites, served with a tangy Lorem Ipsum aioli. Duis non tortor a lectus consectetur fringilla. Sed lobortis arcu ut vestibulum elementum.";
+}
+
+function setSpecialsTextBox1() {
+    const specialsContent0 = document.querySelector('.specials-content-0');
+    const specialsContent1 = document.querySelector('.specials-content-1');
+    const h3 = document.createElement('h3');
+    const p = document. createElement('p');
+    specialsContent0.appendChild(h3).textContent =  'Lorem Ipsum \r\n Bliss Burger'
+    specialsContent1.appendChild(p).textContent = 'A mouthwatering blend of char-grilled Lorem Ipsum-infused beef patty, topped with Lorem Ipsum bacon jam, aged cheddar, and crisp lettuce on a brioche bun. Sed vitae neque a arcu eleifend ullamcorper. Nullam sagittis ipsum ut semper bibendum.'
+}
+
+function setSpecialsTextBox2 () {
+    const specialsContent2 = document.querySelector('.specials-content-2');
+    const specialsContent3 = document.querySelector('.specials-content-3');
+    const h3 = document.createElement('h3');
+    const p = document. createElement('p');
+    specialsContent2.appendChild(h3).textContent = 'Quisque Eget \r\n Elegance Risotto';
+    specialsContent3.appendChild(p).textContent = 'Creamy Arborio rice cooked to perfection, mixed with wild mushrooms, truffle oil, and a sprinkle of Lorem Ipsum dust. In hendrerit felis eu lorem ipsum creaminess. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.';
+}
+
+function setSpecialsTextBox3 () {
+    const specialsContent4 = document.querySelector('.specials-content-4');
+    const specialsContent5 = document.querySelector('.specials-content-5');
+    const h3 = document.createElement('h3');
+    const p = document. createElement('p');
+    specialsContent4.appendChild(h3).textContent = 'Lorem Ipsum \r\n Fusion Tacos';
+    specialsContent5.appendChild(p).textContent = 'A culinary adventure with Korean BBQ-inspired Lorem Ipsum-marinated grilled chicken, pickled slaw, and spicy Lorem Ipsum aioli, all nestled in warm corn tortillas. Phasellus hendrerit turpis vitae libero bibendum, eu pellentesque sapien fermentum. Ut feugiat ligula id ligula luctus, non ullamcorper dolor hendrerit.';
+}
+
+function setSidesTextBox1() {
+    const sidesContent4 = document.querySelector('.sides-content-0');
+    const sidesContent5 = document.querySelector('.sides-content-1');
+    const h3 = document.createElement('h3');
+    const p = document. createElement('p');
+    sidesContent4.appendChild(h3).textContent = 'Quisque Ipsum \r\n Garlic Knots';
+    sidesContent5.appendChild(p).textContent = 'Soft and pillowy garlic knots brushed with Lorem Ipsum garlic butter, served with a side of marinara sauce. Duis non tortor a lectus consectetur fringilla. Sed lobortis arcu ut vestibulum elementum.';
+}
+
+function setSidesTextBox2() {
+    const sidesContent4 = document.querySelector('.sides-content-2');
+    const sidesContent5 = document.querySelector('.sides-content-3');
+    const h3 = document.createElement('h3');
+    const p = document. createElement('p');
+    sidesContent4.appendChild(h3).textContent = 'Sed Ipsum Mediterranean \r\n Couscous Salad';
+    sidesContent5.appendChild(p).textContent = 'A refreshing salad with fluffy couscous, cherry tomatoes, cucumber, feta cheese, and a tangy Lorem Ipsum vinaigrette. Vestibulum ac aliquam nisl. Sed vitae neque a arcu eleifend ullamcorper. Nullam sagittis ipsum ut semper bibendum.';
+}
+
+function setSidesTextBox3() {
+    const sidesContent4 = document.querySelector('.sides-content-4');
+    const sidesContent5 = document.querySelector('.sides-content-5');
+    const h3 = document.createElement('h3');
+    const p = document. createElement('p');
+    sidesContent4.appendChild(h3).textContent = 'Lorem Ipsum Parmesan \r\n Truffle Fries';
+    sidesContent5.appendChild(p).textContent = 'Crispy golden fries tossed with Parmesan cheese and a drizzle of Lorem Ipsum-infused truffle oil. Sed vitae neque a arcu eleifend ullamcorper. Nullam sagittis ipsum ut semper bibendum.';
 }
 
 function giveDiffClasses(elements) {
@@ -121,10 +178,6 @@ function giveDiffClasses(elements) {
     for(let i = 0; i < selection.length; i++){
         selection[i].classList.add(`${elements}-${i}`);
     }
-}
-
-function setSpecialText() {
-
 }
 
 export function appendGeneratedH2(parent, numOfH2s, className){
